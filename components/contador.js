4a5ad1f2e0dc101b421b7a0e75b35abe93b9f471
Oscar Bautista <oscar.adba@gmail.com>
2021-10-19 18:54:15 -0700

    Crear componente contador

---
+++ components/contador.js
@@ -0,0 +1,18 @@
+const Contador = () => {
+  const [contador, setContador] = React.useState(0);
+
+  const aumentar = () => setContador(contador + 1);
+  const disminuir = () => setContador(contador - 1);
+
+  return (
+    <div>
+      <h1>
+        Contador
+        <span className={contador >= 0 ? "mayor" : "menor"}>{contador}</span>
+      </h1>
+      <hr />
+      <button onClick={aumentar}>Aumentar</button>
+      <button onClick={disminuir}>Disminuir</button>
+    </div>
+  );
+};
